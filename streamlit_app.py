be127f4ac195fc83351cd24b05dabf8c45989e51
aimaanmansuri21 <97092357+aimaanmansuri21@users.noreply.github.com>
2025-06-24 22:28:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.markdown("""
             background-color: #ff0000;
             color: white;
             padding: 0.75em 1.5em;
-            font-size: 0; /* Hide default text */
+            font-size: 2rem;
             font-weight: 600;
             border: none;
             border-radius: 8px;
@@ -34,9 +34,6 @@ st.markdown("""
         }
         .stButton>button:hover {
             background-color: #cc0000;
-        }
-        .stButton>button span.emoji {
-            font-size: 2rem; /* Enlarged emoji */
         }
         .block-container {
             padding: 2rem 3rem;
@@ -64,7 +61,7 @@ with st.container():
         )
     with col2:
         st.markdown("<div style='padding-top: 32px;'>", unsafe_allow_html=True)
-        if st.button("<span class='emoji'>🎲</span>", key="randomize_btn"):
+        if st.button("🎲", key="randomize_btn"):
             niche = random.choice(list(niche_keywords.keys()))
             keywords = random.sample(niche_keywords[niche], min(5, len(niche_keywords[niche])))
             st.session_state["keyword_input"] = ", ".join(keywords)
